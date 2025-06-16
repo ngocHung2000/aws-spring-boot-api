93200138189aa97e66dc97bde9e47e7fd28af62c
hungtn1 <hungtn1@hdbank.com.vn>
2025-06-16 10:20:00 +0700

    hungtn

---
+++ src/main/java/com/vn/awsspringbootapi/AwsSpringBootApiApplication.java
@@ -0,0 +1,13 @@
+package com.vn.awsspringbootapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSpringBootApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsSpringBootApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vn/awsspringbootapi/AwsSpringBootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vn.awsspringbootapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSpringBootApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
