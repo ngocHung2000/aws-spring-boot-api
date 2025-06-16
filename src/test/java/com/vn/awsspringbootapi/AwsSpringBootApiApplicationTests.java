e101648526c5d169ef42e56545b7be3fa0b79cba
hungtn1 <hungtn1@hdbank.com.vn>
2025-06-16 11:03:03 +0700

    update test rest api

--- src/test/java/com/vn/awsspringbootapi/AwsSpringBootApiApplicationTests.java
+++ src/test/java/com/vn/awsspringbootapi/AwsSpringBootApiApplicationTests.java
@@ -1,13 +1,35 @@
 package com.vn.awsspringbootapi;
 
+import com.vn.awsspringbootapi.controllers.HelloWorldController;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AwsSpringBootApiApplicationTests {
 
+    @Autowired
+    private HelloWorldController helloWorldController;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @LocalServerPort
+    private int port;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+        assertThat(helloWorldController).isNotNull();
     }
 
+    @Test
+    void greetingShouldReturnDefaultMessageForHelloRestAPI() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/api/v1/hello",
+                String.class)).contains("Hello World!");
+    }
 }
